6a23d9254c99fafc08762d266fa6419238e33f8e
brianma94 <brian_1994_bcn@hotmail.com>
2015-11-20 01:07:13 +0100

    Create ControllerBoard.java

---
+++ propias/dominio/controladores/ControllerBoard.java
@@ -0,0 +1,50 @@
+package propias.dominio.controladores;
+import propias.dominio.clases.SudokuSolver;
+import propias.dominio.clases.SudokuVerification;
+import propias.dominio.clases.Board;
+/**
+ * 
+ * @author Brian
+ *
+ */
+public class ControllerBoard {
+	/**
+	 * 
+	 * @param m
+	 */
+	public ControllerBoard() {
+		
+	}
+	Board convertMatrixToBoard(int[][] m){
+		Board b = null;
+		try {
+			b = new Board(m[0].length);
+		} catch (Exception e1) {
+		}
+    	for (int i=0; i<m[0].length; ++i){
+    		for(int j=0; j<m[0].length; ++i) {
+    			try {
+					b.setCellValue(i, j, m[i][j]);
+				} catch (Exception e) {
+				} 
+    		}
+    	}
+    	return b;
+	}
+	/**
+	 * 
+	 * @param m
+	 * @return
+	 */
+	public boolean verify(int[][] m){
+		Board b = convertMatrixToBoard(m);
+		SudokuVerification s = new SudokuVerification();
+		boolean solved = s.resolve(b);
+		SudokuSolver sol = new SudokuSolver(b);
+		sol.needUnique();
+		sol.solve();
+		boolean unique = sol.isUnique();
+		if (unique && solved) return true;
+		return false;
+	}
+}
