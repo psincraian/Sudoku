3ef933e62fb2369d0e51bb25478d5d1e2c85ec41
Adrian Sanchez Albanell <adrian.sanchez.albanell@a5s105pc12.fib.upc.es>
2015-11-23 17:50:34 +0100

    funciona!

--- propias/presentacion/ViewProfile.java
+++ propias/presentacion/ViewProfile.java
@@ -25,8 +25,8 @@ public class ViewProfile {
 	public void showProfile(){
 		System.out.println("PARTIDES JUGADES");
 		System.out.println("Partides Facils Jugades: " + m[0]);
-		System.out.println("Partides Facils Jugades: " + m[1]);
-		System.out.println("Partides Facils Jugades: " + m[2] + "\n");
+		System.out.println("Partides Mitjanes Jugades: " + m[1]);
+		System.out.println("Partides Dificils Jugades: " + m[2] + "\n");
 		System.out.println("TEMPS TOTAL");
 		System.out.println("Temps Partides Facils: " + t[0]);
 		System.out.println("Temps Partides Mitjanes: " + t[1]);
@@ -34,6 +34,6 @@ public class ViewProfile {
 		System.out.println("TEMPS TOTAL");
 		System.out.println("Millor Temps Partides Facils: " + bt[0]);
 		System.out.println("Millor Temps Partides Mitjanes: " + bt[1]);
-		System.out.println("Temps Partides Dificils: " + bt[2] + "\n");
+		System.out.println("Millor Temps Partides Dificils: " + bt[2] + "\n");
 	}
 }
