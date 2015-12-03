1058b07e6c6db7349663a3b46b271b34760f51e9
brianma94 <brian_1994_bcn@hotmail.com>
2015-12-04 00:55:34 +0100

    It works!

--- propias/presentacion/ViewLoadMatch.java
+++ propias/presentacion/ViewLoadMatch.java
@@ -1,38 +1,74 @@
 package propias.presentacion;
 
 import java.util.List;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
-public class ViewLoadMatch {
-	public ViewLoadMatch(){
-		
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ViewLoadMatch extends SetView{
+	List<String> id;
+	List<JButton> buttonList = new ArrayList<JButton>();
+	public ViewLoadMatch(List<String> id){
+		super();
+		setTitle("Carregar Partida");
+		this.id = id;
+		startGUI();
 	}
-    public int getLoadMatch(List<String> id){
-		       showLoadMatch(id);
-		       if (id.size() != 0){
-			       Scanner scanner = new Scanner(System.in);
-			       int op = 1;
-			       op = scanner.nextInt();	
-			       while(op <=0 || op > id.size())	op = scanner.nextInt();
-			       return op;
-		       }
-		       else return 0;
- 	  }
 	
-   	public void showLoadMatch(List<String> id) {
-   		
-   		if (id.size() == 0) System.out.println("No hi ha partides guardades");
-   		else {
-   			System.out.println("Quina partida vols continuar jugant? ");
-                   Iterator<String> iter = id.iterator();
-                   int i = 1;
-            while(iter.hasNext())  {
-            	System.out.println(i + ". " + iter.next() + "\n");
-            	++i;
-            }
-   		}
-   	}
+	public void startGUI() {
+		instanceGUI();
+		configureGUI();
+		addToGUI();
+		pack();
+		setVisible(true);
+	}
+	private void instanceGUI() {
+	}
+	/**
+	 * Configura la vista
+	 */
+	private void configureGUI() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		dispose();
+	}
+	/**
+	 * inclou a la vista els components
+	 */
+	private void addToGUI() {
+		Box vertical = Box.createVerticalBox();
+		vertical.add(Box.createVerticalGlue());
+		JPanel panelButtons = new JPanel();
+		for(int i=0; i<id.size(); ++i) {
+			JButton b = new JButton(id.get(i));
+			buttonList.add(b);
+			panelButtons.add(b);
+		}
+		panelButtons.setBackground(Color.WHITE);
+		panelButtons.setLayout(new GridLayout(id.size(),0));
+		getContentPane().add(panelButtons, BorderLayout.CENTER);
+	}
+	/**
+	 * 
+	 * @param ma Comproba si el boto s'ha premut
+	 * @param button Boto a premer
+	 */
+	public void listeners(MouseAdapter ma, JButton button){
+		button.addMouseListener(ma);
+	}
+	/**
+	 * Deixa la vista com a no visible
+	 */
+	public void disableView(){
+		setVisible(false);
+	}
 	
 }
