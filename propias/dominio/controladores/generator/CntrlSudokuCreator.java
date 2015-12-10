a2c5f2128e6add154896e4ea19b9905660b2b161
adrian729 <adrian.sal@gmail.com>
2015-12-10 14:12:12 +0100

    Update CntrlSudokuCreator.java
    afegida dificultat al sudoku creat (faltaba ferho)

--- propias/dominio/controladores/generator/CntrlSudokuCreator.java
+++ propias/dominio/controladores/generator/CntrlSudokuCreator.java
@@ -71,7 +71,7 @@ public class CntrlSudokuCreator {
 	    }
 	    randomizeSudoku(rand.nextInt(10)*2000);	 
 	    setTypeCells();   
-	    return new Sudoku(sudoku, solution);
+	    return new Sudoku(sudoku, solution, (dificultat+1));
 	}
 
 	/**
