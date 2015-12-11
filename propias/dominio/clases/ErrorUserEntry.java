62a1cb0a064787c3477eaa5321ffe7cbc8a3691a
danielsm2 <daniel.sanchez.martinez@a5s104mac51.fib.upc.es>
2015-12-11 19:08:45 +0100

    add enum

---
+++ propias/dominio/clases/ErrorUserEntry.java
@@ -0,0 +1,17 @@
+package propias.dominio.clases;
+
+/**
+ * 
+ * @author Daniel Sanchez Martinez
+ * 
+ * Indica els possibles tipus de error al introduir un 
+ * usuari nou o ja loguejat
+ */
+public enum ErrorUserEntry {
+	LOGIN_OK,
+	LOGIN_FAIL_USER_EMPTY,
+	LOGIN_FAIL_USER_EXISTS,
+	LOGIN_FAIL_EMPTY_PASSWORDS,
+	LOGIN_FAIL_PASSWORDS_DISTINCT,
+	LOGIN_FAIL_ONLY_VALID_CHARS
+}
