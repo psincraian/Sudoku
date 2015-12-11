e2b3f16083cebbf5577cb1d257db4bd2b1f72aab
brianma94 <brian_1994_bcn@hotmail.com>
2015-12-11 15:39:44 +0100

    modified

--- propias/persistencia/ControllerPersistance.java
+++ propias/persistencia/ControllerPersistance.java
@@ -19,8 +19,21 @@ public class ControllerPersistance {
 	
 	public ControllerPersistance(){
 		autoPath = Paths.get("").toAbsolutePath().toString();
+		File d = new File(autoPath + "/data");
+		if(!d.exists()){
+			new File(autoPath + "/data/Ranking/").mkdirs();
+			new File(autoPath + "/data/Users/").mkdirs();
+			new File(autoPath + "/data/Sudokus/9x9/").mkdirs();
+			new File(autoPath + "/data/Sudokus/9x9/Facil/").mkdirs();			
+			new File(autoPath + "/data/Sudokus/9x9/Medio/").mkdirs();
+			new File(autoPath + "/data/Sudokus/9x9/Dificil/").mkdirs();
+			new File(autoPath + "/data/Sudokus/16x16/").mkdirs();
+			new File(autoPath + "/data/Sudokus/16x16/Facil/").mkdirs();
+			new File(autoPath + "/data/Sudokus/16x16/Medio/").mkdirs();
+			new File(autoPath + "/data/Sudokus/16x16/Dificil/").mkdirs();
+		}
 	}
-
+	
 	/**
 	*Creadora per a fer servir amb el drivers
 	*(ja que esta en una ubicacio diferent)
@@ -28,74 +41,9 @@ public class ControllerPersistance {
 	*/
 	public ControllerPersistance(boolean driver){
 		autoPath = Paths.get("").toAbsolutePath().toString();
-		if(driver){			
+		if(driver){
 			autoPath = autoPath + "/../../";
 		}
-	}
-
-	/**
-   	* Converteix un sudoku en format int[][] a una String
-   	* amb format: 
-  	* 7.4.....2...8.1...3.........5.6..1..2...4...........5...
-  	* .37.....9....6...8.....9.
-  	* Els nombres superiors a 10 els representa amb lletres
-  	* (A, B, C, etc).
-  	* @param sudoku matriu a convertir en String.
-  	* @return String representant el sudoku.
-  	*/
-	public String sudokuToString(int[][] sudoku){
-		String sudo = new String();
-		int size = sudoku.length;
-		for(int i = 0; i < size; ++i){
-			for(int j = 0; j < size; ++j){
-				if(sudoku[i][j] == 0){
-					sudo = sudo + '.';
-				}
-				else{
-					int num = sudoku[i][j];
-					if(num < 10){
-						sudo = sudo + Integer.toString(num);
-					}
-					else if(num == 16){
-						sudo = sudo + "G";
-					}
-					else{
-						sudo = sudo + 
-						Integer.toHexString(num).toUpperCase();
-					}
-					
-				}
-			}
-		}
-		return sudo;
-	}
-
-	/**
-   	* Converteix un sudoku en format String
-   	* de la forma: 
-  	* 7.4.....2...8.1...3.........5.6..1..2...4...........5...
-  	* .37.....9....6...8.....9.
-  	* On els nombres superiors a 10 els representa amb lletres
-  	* (A, B, C, etc).
-  	* En una matriu d'enters.
-  	* @param sudoku String a convertir en int[][].
-  	* @return int[][] representant el sudoku.
-  	*/
-	public static int[][] stringToMatrix(String s) throws Exception {
-		int size = (int) Math.sqrt(s.length());
-		int[][] board = new int[size][size];
-		for(int i = 0; i < size*size; i++){
-		    char c = s.charAt(i);
-		    int row = i / size;
-		    int col = i % size;
-		    if(c != '.' && c >= '1' && c <= '9'){
-		        board[row][col] = c - '0';
-		    }
-	        else if (c != '.'){
-		        board[row][col] = c - 'A' + 10;
-	        }
-		}		
-	    return board;
 	}
 
 	/**
@@ -105,31 +53,21 @@ public class ControllerPersistance {
   	*/
 	public boolean existsUser(String user) {
 		File u = new File(autoPath + "/data/Users/" + user);
-			    
-	    //Path currentRelativePath = Paths.get("").toAbsolutePath().toString();
-		//String s = currentRelativePath.toAbsolutePath().toString();
-		//System.out.println("Current relative path is: " + s);
-		//System.out.println("PATH: " + autoPath + "/data/Users/" + user);
 		return u.exists();
 	}
 
 	/**
-   	* Converteix un sudoku en format int[][] a una String
-   	* amb format: 
-  	* 7.4.....2...8.1...3.........5.6..1..2...4...........5...
-  	* .37.....9....6...8.....9.
-  	* Els nombres superiors a 10 els representa amb lletres
-  	* (A, B, C, etc).
-  	* @param sudoku matriu a convertir en String.
-  	* @return String representant el sudoku.
+   	* Metode que retorna un usuari.
+  	* @param user nom de l'usuari.
+  	* @return String representant l'usuari.
   	*/
-	public String getPasswordUser(String user) throws Exception {
-		String password = new String();
-		FileReader fr = new FileReader(autoPath + "/data/Users/" + user + "/" + "infoUser.txt");
+	public String getUser(String user) throws Exception {
+		FileReader fr = new FileReader(autoPath + "/data/Users/" + user + "/user");
 		Scanner scn = new Scanner(fr);
-		password = scn.next();
+		String userObj = scn.next();
+		scn.close();
 		fr.close();
-		return password;
+		return userObj;
 	}
 
 	/**
@@ -142,122 +80,102 @@ public class ControllerPersistance {
 	}
 
 	/**
-   	* A partir d'una matriu que representi el sudoku i una altra
-   	* que representi la solucio, amb la dificultat de sudoku
-   	* corresponent, et guarda el sudoku amb el format
-  	* 7.4.....2...8.1...3.........5.6..1..2...4...........5...
-  	* .37.....9....6...8.....9.
-  	* per a cada matriu donantli un identificador unic al
-  	* fitxer on es guardarà.
-  	* Els nombres superiors a 10 els representa amb lletres
-  	* (A, B, C, etc).
-  	* @param sudoku matriu a convertir en String.
-  	* @return String representant el sudoku.
-  	*/
-	public void introduceSudoku(int[][] sudoku, int[][] solution, int dif) throws Exception{
-		String difSt;
-		if(dif == 0){
-			difSt = "Facil";
-		}
-		else if(dif == 1){
-			difSt = "Medio";
-		}
-		else{
-			difSt = "Dificil";
-		}
-		String size = Integer.toString(sudoku.length);
-		String dir = autoPath + "/data/Sudokus/" + size + "x" + size + "/" + difSt;
-		File fl = new File(dir);
-		String id = size + "s" + Integer.toString(fl.list().length + 1) + "l" + dif;
-		FileWriter fw = new FileWriter(autoPath + "/data/Sudokus/" + size + "x" + size + "/" + difSt + "/" + id +".txt");
-		PrintWriter pw = new PrintWriter(fw);
-		pw.println(sudokuToString(sudoku));
-		pw.println(sudokuToString(solution));
-		fw.close();
-		FileWriter fwRank = new FileWriter(autoPath + "/data/Ranking/" + id + ".txt");
-		fwRank.close();
-	}
-
-	/**
    	* Permet la creacio d'un nou usuari
    	* (i tots els directoris, arxius
    	*  i modificacions d'arxius
    	*  corresponents).
   	* @param name String amb el nom d'usuari.
-  	* @param password String amb la password d'usuari.
+  	* @param user l'usuari.
   	*/
-	public void newUser(String name, String password) throws Exception {
+	public void newUser(String name, String user, String stadistics) throws Exception {
 		new File(autoPath + "/data/Users/" + name + "/Partidas/").mkdirs();
-		File info = new File(autoPath + "/data/Users/" + name + "/" + "infoUser.txt");
-		info.createNewFile();
-		FileWriter fw = new FileWriter(autoPath + "/data/Users/" + name + "/infoUser.txt");
+		File userObj = new File(autoPath + "/data/Users/" + name + "/user");
+		userObj.createNewFile();
+		FileWriter fw = new FileWriter(autoPath + "/data/Users/" + name + "/user");
 		PrintWriter pw = new PrintWriter(fw);
-		pw.println(password);
-		for(int i = 0; i < 9; ++i){
-			pw.println("0");
-		}
-		fw.close();		
-	    List<List<String>> rankingGlob = new  ArrayList<List<String>>(getRanking("RankingGlobal"));
-	    List<String> userRank = new ArrayList<String>();
-	    userRank.add(name);
-	    userRank.add("0");
-	    rankingGlob.add(userRank);
-	    setRanking("RankingGlobal", rankingGlob);
+		pw.print(user);
+		pw.close();
+		fw.close();
+		File userStadistics = new File(autoPath + "/data/Users/" + name + "/stadistics");
+		userStadistics.createNewFile();
+		FileWriter fw2 = new FileWriter(autoPath + "/data/Users/" + name + "/stadistics");
+		PrintWriter pw2 = new PrintWriter(fw2);
+		pw2.print(stadistics);
+		pw2.close();
+		fw2.close();
 	}
 
 	/**
    	* Permet obtenir les estadistiques de l'usuari
    	* carregat al controlador.
-  	* @return List<Long> amb nombre de partides facils,
-  	* mitjanes i dificils jugades, temps dedicat a partides 
-  	* facils, mitjanes i dificils i millor temps en 
-  	* partides mitjanes, facils i dificils.
+   	* @return String estadistiques serialitzades en
+   	* una String.
   	*/
-	public List<Long> getStadistics() throws Exception {
-		    List<Long> stadistics = new ArrayList<Long>();
-		    FileReader fr = new FileReader(autoPath + "/data/Users/" + user + "/" + "infoUser.txt");
+	public String getStadistics() throws Exception {
+		    FileReader fr = new FileReader(autoPath + "/data/Users/" + user + "/stadistics");
 			Scanner scn = new Scanner(fr);
-			scn.next();
-			for(int i = 0; i < 9; ++i){
-				stadistics.add(Long.parseLong(scn.next(), 10));
-			}
+			String std = scn.next();
 			fr.close();
-		    return stadistics;
+		    return std;
 	}
 
 	/**
    	* Permet actualitzar les estadistiques de l'usuari
    	* carregat al controlador.
-  	* @param List<Long> amb nombre de partides facils,
-  	* mitjanes i dificils jugades, temps dedicat a partides 
-  	* facils, mitjanes i dificils i millor temps en 
-  	* partides mitjanes, facils i dificils.
+  	* @param stadistics noves estadistiques.
   	*/
-	public void setStadistics(List<Long> stadistics) throws Exception {
-			FileReader fr = new FileReader(autoPath + "/data/Users/" + user + "/" + "infoUser.txt");
-			Scanner scn = new Scanner(fr);
-			String password = scn.next();
-			fr.close();
-		    FileWriter fw = new FileWriter(autoPath + "/data/Users/" + user + "/" + "infoUser.txt");
+	public void setStadistics(String stadistics) throws Exception {
+		    FileWriter fw = new FileWriter(autoPath + "/data/Users/" + user + "/stadistics");
 		    PrintWriter pw = new PrintWriter(fw);
-		    pw.println(password);
-		    for(int i = 0; i < stadistics.size(); ++i){
-		    	pw.println(stadistics.get(i));
-		    }
+		    pw.print(stadistics);
 		    fw.close();
 	}
 
 	/**
-   	* Permet obtenir un sudoku d'una certa mida i
+   	* Introdueix un nou sudoku als fitxers.
+  	* @param sudoku sudoku a guardar.
+  	* @param dif dificultat del sudoku.
+  	* @param size mida del sudoku.
+  	*/
+	public String introduceSudoku(String sudoku, int dif, int size) throws Exception {
+		String difSt;
+		String difID;
+		if(dif == 0){
+			difSt = "Facil";
+			difID = "e";
+		}
+		else if(dif == 1){
+			difSt = "Medio";
+			difID = "m";
+		}
+		else{
+			difSt = "Dificil";
+			difID = "d";
+		}
+		String sizeStr = Integer.toString(size);
+		String dir = autoPath + "/data/Sudokus/" + size + "x" + size + "/" + difSt;
+		File fl = new File(dir);		
+		if(size == 16){
+			difID = difID.toUpperCase();
+		}
+		String id = difID + Integer.toString(fl.list().length + 1);
+		FileWriter fw = new FileWriter(autoPath + "/data/Sudokus/" + size + "x" + size + "/" + difSt + "/" + id);
+		PrintWriter pw = new PrintWriter(fw);
+		pw.print(sudoku);
+		fw.close();
+		return id;
+	}
+
+	/**
+   	* Permet obtenir el ID d'un sudoku d'una certa mida i
    	* dificultat.
    	* @param size mida del sudoku.
    	* @param dif dificultat del sudoku(0, 1 o 2 de menor
    	* a major dificultat).
-  	* @return List<String> llista amb la ID del sudoku,
-  	* el sudoku amb forats i la solucio al sudoku.
+  	* @return String la ID d'un sudoku que cumpleixi
+  	* la mida i dificultat donades.
   	*/
-	public List<String> getSudoku(int size, int dif) throws Exception {
-		List<String> infoSudoku = new ArrayList<String>();
+	public String getIDSudokuRandom(int size, int dif) throws Exception {
 		String difSt;
 		if(dif == 0){
 			difSt = "Facil";
@@ -272,14 +190,63 @@ public class ControllerPersistance {
 		File[] sudokus = f.listFiles();
 		Random rand = new Random();
 		int pos = rand.nextInt(sudokus.length);
-		infoSudoku.add(sudokus[pos].getName());
-		FileReader fr = new FileReader(sudokus[pos].getAbsolutePath());
-		Scanner scn = new Scanner(fr);
-		while(scn.hasNext()){
-			infoSudoku.add(scn.next());
+		return sudokus[pos].getName();
+	}
+
+	/**
+	* Permet obtenir tots els
+	* identificadors de sudoku
+	* dels sudokus amb una certa
+	* mida i nivell de dificultat.
+	* @param size la mida.
+	* @param dif la dificultat.
+	* @return List<String> amb
+	* els identificadors dels
+	* sudokus.
+	*/
+	public List<String> getIDSudokus(int size, int dif){
+		String difSt;
+		if(dif == 0){
+			difSt = "Facil";
 		}
-		fr.close();
-		return infoSudoku;
+		else if(dif == 1){
+			difSt = "Medio";
+		}
+		else{
+			difSt = "Dificil";
+		}
+		File f = new File(autoPath + "/data/Sudokus/" + size + "x" + size + "/" + difSt);
+		File[] sudokus = f.listFiles();
+		List<String> idSudokus = new ArrayList<String>();
+		for(int a = 0; a < sudokus.length; ++a){
+			idSudokus.add(sudokus[a].getName());
+		}
+		return idSudokus;
+	}
+
+	/**
+	* Permet obtenir el sudoku amb
+	* la id donada.
+	* @param id identificador del sudoku.
+	* @param size mida del sudoku.
+	* @param dif dificultat del sudoku.
+	* @return String sudoku serialitzat.
+	*/
+	public String getSudoku(String id, int size, int dif) throws Exception {
+		String difSt;
+		if(dif == 0){
+			difSt = "Facil";
+		}
+		else if(dif == 1){
+			difSt = "Medio";
+		}
+		else{
+			difSt = "Dificil";
+		}
+		FileReader fr = new FileReader(autoPath + "/data/Sudokus/" + size + "x" + size + "/" + difSt + "/" + id);
+		Scanner scn = new Scanner(fr);
+		String sudo = scn.next();
+		return sudo;
 	}
 
 	/**
@@ -298,26 +265,14 @@ public class ControllerPersistance {
 			return savedMatches;
 	}
 
-	/*
-		devuelve 3 matrices: actual de la partida, inicial con agujeros, solucion.
-	*/
 	/**
-   	* Retorna una llista de tres matrius que representen 
-   	* una partida a mitges:
-   	* Una matriu amb el progres actual de la partida.
-   	* una matriu amb els valors inicials del sudoku.
-   	* una matriu amb la solucio al sudoku.
-   	* l'usuari carregat al controlador.
-  	* @return List<String> retorna els identificadors de
-  	* les partides.
+   	* Retorna una partida.
+  	* @return String partida serialitzada.
   	*/
-	public List<int[][]> getSavedMatch(String idMatch) throws Exception {
-		List<int[][]> savedMatch = new ArrayList<int[][]>();
+	public String getSavedMatch(String idMatch) throws Exception {
 		FileReader fr = new FileReader(autoPath + "/data/Users/" + user + "/Partidas/" + idMatch);
 		Scanner scn = new Scanner(fr);
-		while(scn.hasNext()){
-			savedMatch.add(stringToMatrix(scn.next()));
-		}
+		String savedMatch = scn.next();
 		fr.close();
 		return savedMatch;
 	}
@@ -327,61 +282,51 @@ public class ControllerPersistance {
    	* @param idSudoku id del sudoku a guardar.
    	* @param match partida.
   	*/
-	public void saveMatch(String idSudoku, List<int[][]> match) throws Exception {
+	public void saveMatch(String idSudoku, String match) throws Exception {
 		File f = new File(autoPath + "/data/Users/" + user + "/Partidas/" + idSudoku);
 		f.delete();
 		f.createNewFile();
 		FileWriter fw = new FileWriter(f.getAbsolutePath());
 		PrintWriter pw = new PrintWriter(fw);
-		String sudoku;
-		for(int i = 0; i < match.size(); ++i){
-			sudoku = sudokuToString(match.get(i));
-			pw.println(sudoku);
-		}
+		pw.print(match);
 		fw.close();
 	}
 
 	/**
-   	* Retorna un ranking, ja sigui RankingGlobal o el d'un sudoku. 
-  	* @param id identificador del sudoku o RankingGlobal en cas de 
-    * ranking global.
-  	* @return List<List<String>> retorna la representacio del ranking
-  	* en llistes on hi ha el nom de l'usuari i la seva puntuacio.
-  	*/
-	public List<List<String>> getRanking(String id) throws Exception {
-		List<List<String>> nameScore = new ArrayList<List<String>>();
-		FileReader fr = new FileReader(autoPath + "/data/Ranking/" + id + ".txt");
-		Scanner scn = new Scanner(fr);
-		Pattern delimiter = Pattern.compile("[;\n]");
-		scn.useDelimiter(delimiter);
-		while(scn.hasNext()){
-			List<String> tmp = new ArrayList<String>();
-			tmp.add(scn.next());
-			tmp.add(scn.next());
-			nameScore.add(tmp);
-		}		
-		fr.close();
-		return nameScore;
+	* Metode per saver si existeix o no
+	* Ranking Global
+	* @return boolean Retorna true si existeix
+	* el RankingGlobal, false altrament.
+	*/
+	public boolean existRankingGlobal(){
+		File u = new File(autoPath + "/data/Ranking/RankingGlobal");
+		return u.exists();
 	}
 
 	/**
-   	* Modifica o crea un ranking.
-  	* @param id identificador del sudoku o RankingGlobal en cas de 
-    * ranking global.
-    * @param ranking valors del ranking, llistes de String
-    * amb nom i puntuacio.
+   	* Retorna el RankingGlobal. 
+  	* @return String RankingGlobal serialitzat.
   	*/
-	public void setRanking(String id, List<List<String>> ranking) throws Exception {
-		File f = new File(autoPath + "/data/Ranking/" + id + ".txt");
+	public String getRankingGlobal() throws Exception {
+		FileReader fr = new FileReader(autoPath + "/data/Ranking/RankingGlobal");
+		Scanner scn = new Scanner(fr);
+		String ranking = scn.next();
+		fr.close();
+		return ranking;
+	}
+
+	/**
+	* Afegeix o modifica RankingGlobal.
+	* @param rankingGLobal nou RankingGlobal.
+  	*/
+	public void setRankingGlobal(String rankingGlobal) throws Exception {
+		File f = new File(autoPath + "/data/Ranking/RankingGlobal");
 		f.delete();
 		f.createNewFile();
 		FileWriter fw = new FileWriter(f.getAbsolutePath());
 		PrintWriter pw = new PrintWriter(fw);
 		String sudoku;
-		for(int i = 0; i < ranking.size(); ++i){
-			List<String> tmp = ranking.get(i);
-			pw.print(tmp.get(0) + ";" + tmp.get(1) + "\n");
-		}
+		pw.print(rankingGlobal);
 		fw.close();
 	}
 }
