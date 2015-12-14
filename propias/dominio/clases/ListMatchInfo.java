6a130cf70eaf381471c022acfdd53b645ea0eb6d
adrian729 <adrian.sal@gmail.com>
2015-12-14 17:24:15 +0100

    Update ListMatchInfo.java

--- propias/dominio/clases/ListMatchInfo.java
+++ propias/dominio/clases/ListMatchInfo.java
@@ -9,63 +9,10 @@ import java.util.*;
  * @author Adrian Sanchez Albanell
  * 
  */
-public class ListMatchInfo implements java.io.Serializable {
-
-	private class infoMatch implements java.io.Serializable {
-		public String name;
-		public String maker;
-		public int givens;
-	};
-
-	private List<infoMatch> listInfo;
+public class ListMatchInfo extends ListInfo implements java.io.Serializable {
 
 	public ListMatchInfo(){
-		listInfo = new ArrayList<infoMatch>();
-	}
-
-	/**
-	* Permet afegir la informacio d'un partnamea
-	* a la llista.
-	* @param name nameentificador de la partnamea.
-	* @param maker creador de la partnamea.
-	* @param givens caselles inicials de la
-	* partnamea.
-	*/
-	public void addInfo(String name, String maker, int givens){
-		infoMatch i = new infoMatch();
-		i.name = name;
-		i.maker = maker;
-		i.givens = givens;
-		int p = searchPosition(name);
-		if(p != -1) {
-			listInfo.set(p, i);
-		}
-		else {
-			listInfo.add(i);
-		}
-	}
-
-	/**
-	* Metode per a borrar una partnamea.
-	*/
-	public void removeInfo(String name){
-		int pos = searchPosition(name);
-		if(pos != -1) listInfo.remove(pos);
-	}
-
-	/**
-	* Permet buscar en quina posicio es
-	* troba una partnamea dins la llista 
-	* d'informacio de les partnamees.
-	* @return int la posicio de la 
-	* partnamea en la llista d'informacio,
-	* -1 si no hi es.
-	*/
-	private int searchPosition(String name){		
-		for(int p = 0; p < listInfo.size(); ++p){
-			if(listInfo.get(p).name.equals(name)) return p;
-		}
-		return -1;
+		super();
 	}
 
 	/**
@@ -80,15 +27,13 @@ public class ListMatchInfo implements java.io.Serializable {
 	*/
 	public List<List<String>> getInfoIdMakerGivens(){
 		List<List<String>> infoIdMaker = new ArrayList<List<String>>();
-		List<String> listGivens = new ArrayList<String>();
-		for(int a = 0; a < listInfo.size(); ++a){			
-				List<String> tmp = new ArrayList<String>();
-				tmp.add(listInfo.get(a).name);
-				tmp.add(listInfo.get(a).maker);
-				infoIdMaker.add(tmp);
-				listGivens.add(String.valueOf(listInfo.get(a).givens));
+		for(int a = 0; a < listInfo.size(); ++a){
+			List<String> tmp = new ArrayList<String>();
+			tmp.add(listInfo.get(a).name);
+			tmp.add(listInfo.get(a).maker);
+			tmp.add(String.valueOf(listInfo.get(a).givens));
+			infoIdMaker.add(tmp);
 		}
-		infoIdMaker.add(listGivens);
 		return infoIdMaker;
 	}
 
